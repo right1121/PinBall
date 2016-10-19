42a6fdc043f449f6906074b6e04d1b2f932a53e1
MsakiHoshino <right1121@hotmail.co.jp>
2016-10-19 16:04:36 +0900

    Lesson5課題　PinBall
    Lesson5課題です。Scoreを右上に表示できるよう、ScoreControllerを作成しました。

---
+++ Assets/FripperController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FripperController : MonoBehaviour {
+    //HingiJointコンポーネントを入れる
+    private HingeJoint myHingeJoynt;
+
+    //初期の傾き
+    private float defaultAnge = 20;
+
+    //はじいた時の傾き
+    private float flickAngle = -20;
+
+	// Use this for initialization
+	void Start () {
+            //HingeJointコンポーネント取得
+            this.myHingeJoynt = GetComponent<HingeJoint>();
+
+            //フリッパーの傾きを設定
+            SetAngle(this.defaultAnge);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+            
+            //左矢印キーを押したとき左フリッパーを動かす
+            if (Input.GetKeyDown(KeyCode.LeftArrow) && tag == "LeftFripperTag")
+            {
+                SetAngle (this.flickAngle);
+            }
+            //右矢印キーを押したとき右フリッパーを動かす
+            if (Input.GetKeyDown(KeyCode.RightArrow) && tag == "RightFripperTag")
+            {
+                SetAngle (this.flickAngle);
+            }
+            //矢印キーが離されたときフリッパーをもとに戻す
+            if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow))
+            {
+            SetAngle(this.defaultAnge);
+            }
+	
+	}
+
+    //フリッパーの傾きを設定
+    public void SetAngle (float angle){
+        JointSpring jointSpr = this.myHingeJoynt.spring;
+        jointSpr.targetPosition = angle;
+        this.myHingeJoynt.spring = jointSpr;
+
+    }
+
+}
---
+++ Assets/ScoreController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour {
+
+    //スコアを格納する変数
+    private int score = 0;
+
+    //スコアを表示するテキスト
+    private GameObject scoreText;
+
+    //tagの名前
+    private string tagName;
+
+    // Use this for initialization
+    void Start() {
+
+        this.scoreText = GameObject.Find("ScoreText");
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        this.scoreText.GetComponent<Text>().text = score.ToString();
+    }
+
+    //衝突時に呼ばれる関数
+    void OnCollisionEnter(Collision other) {
+
+        //タグの名前を取得
+        tagName = other.transform.tag;
+
+        //タグの名前ごとにスコアを設定
+        if (tagName == "SmallStarTag") {
+            //小さい星
+            score += 5;
+        }
+        else if (tagName == "LargeStarTag") {
+            //大きい星
+            score += 10;
+        }
+        else if (tagName == "SmallCloudTag") {
+            //小さい雲
+            score += 20;
+        }
+        else if (tagName == "LargeCloudTag") {
+            //大きい雲
+            score += 30;
+        }
+    }
+}
